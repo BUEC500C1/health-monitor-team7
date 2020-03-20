7b3361d1a4148a4f91a373ee30c01cd89449a6a4
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2020-03-20 17:34:17 -0400

    correctly change global variables

--- pulse.py
+++ pulse.py
@@ -1,17 +1,17 @@
 from random import gauss
-from globals import pulse_flag, pulse_value
+import globals
 
 # Normal pulse rate of an average adult (non athlete): 60-100 beats per minute
 def read_pulse():
 	# Gives a value outside range [60,100] approx. 7% of the time
-	pulse = gauss(80, 11)
-	
+	pulse = gauss(80, 20)
+
 	if pulse < 60 or pulse > 100:
 		# Set flag to True when pulse is not normal
-		pulse_flag = True
-		pulse_value = pulse
-	elif (pulse_flag == True):
+		globals.pulse_flag = True
+		globals.pulse_value = pulse
+	elif (globals.pulse_flag == True):
 		# Set flag to False when pulse is back to normal
-		pulse_flag = False
+		globals.pulse_flag = False
 
 	return pulse
