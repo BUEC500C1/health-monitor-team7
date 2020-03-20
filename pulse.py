b1f9c8f2dededbfb0c12fc2433e909ccb1c80ed7
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2020-03-20 16:45:19 -0400

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -3,7 +3,7 @@ from globals import pulse_flag, pulse_value
 
 # Normal pulse rate of an average adult (non athlete): 60-100 beats per minute
 def read_pulse:
-	# Gives a value outside range [60,100] approx. 6.3% of the time
+	# Gives a value outside range [60,100] approx. 7% of the time
 	pulse = gauss(80, 11)
 	if pulse < 60 or pulse > 100:
 		pulse_flag = True
