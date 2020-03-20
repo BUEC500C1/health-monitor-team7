b0e77ab835d5b062c5cc52a9640675a15d89dc05
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2020-03-20 16:59:07 -0400

    updated pulse.py

--- pulse.py
+++ pulse.py
@@ -5,7 +5,13 @@ from globals import pulse_flag, pulse_value
 def read_pulse:
 	# Gives a value outside range [60,100] approx. 7% of the time
 	pulse = gauss(80, 11)
+	
 	if pulse < 60 or pulse > 100:
+		# Set flag to True when pulse is not normal
 		pulse_flag = True
 		pulse_value = pulse
+	elif (flag == True):
+		# Set flag to False when pulse is back to normal
+		pulse_flag = False
+
 	return pulse
