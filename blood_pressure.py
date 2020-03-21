732db49d009ee0db86baef20ec78c4429b50ff80
jesspb <42941981+jesspb@users.noreply.github.com>
2020-03-21 02:19:58 -0400

    Update blood_pressure.py

--- blood_pressure.py
+++ blood_pressure.py
@@ -1,13 +1,13 @@
 import globals
-from random import randint
+from random import gauss
 
 
 def get_bp():
 
     # sys reperesents the systolic blood pressure (numerator)
     # dia reperesents the diastolic blood pressure (denominator)
-    sys = randint(100,140)
-    dia = randint(65,90)
+    sys = round(gauss(120,15))
+    dia = round(gauss(70,15))
 
 
     # blood pressure starting at 130/80 is considered at risk of hypertension
