3003eb8be57e580f5aaab48fe61d88dacfcbedd5
Laura Joy Erb <laurajoy@bu.edu>
2020-03-27 12:22:58 -0400

    added alerts variable to blood oxygen module

--- blood_oxygen.py
+++ blood_oxygen.py
@@ -1,6 +1,16 @@
 import random
+import globals
 
 def read_blood_oxygen():
     # a triangular distribution was chosen to specify the maximum and minimum limits
     # while maintaining a distribution more realistic than a uniform distribution
-    return str(random.triangular(0.90, 1.00, 0.99))
+    oxygen = random.triangular(0.90, 1.00, 0.99)
+
+    # a blood oxygen value below 0.93 is considered unhealthy
+    if oxygen < 0.93:
+        globals.oxygen_flag = True
+        globals.oxygen_value = oxygen
+    else:
+        globals.oxygen_flag = False
+
+    return str(oxygen)
